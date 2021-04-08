1aeb9b656a7d5bf0788457c570540027d27add3f
evgenysobko <jooohn127@gmail.com>
2021-04-08 23:19:27 +0300

    adds host

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,16 +1,15 @@
 import database.DatabaseFactory
 import entities.getParking
 import entities.getUsers
-import io.ktor.application.call
-import io.ktor.request.*
+import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main() {
     DatabaseFactory.init()
-    embeddedServer(Netty, port = 8080) {
+    embeddedServer(Netty, port = 8006, "bulochka.duckdns.org") {
         routing {
             get("/users") {
                 call.respond("users = ${getUsers()}")
