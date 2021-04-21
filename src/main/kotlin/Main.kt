44b8484d593532f29b03e0736b38ba49a792f082
evgenysobko <jooohn127@gmail.com>
2021-04-21 22:40:47 +0300

    edit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,55 @@
+
+import database.DatabaseFactory
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import utils.DateTime
+import utils.Respond
+import utils.Validator
+
+class Main {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            DatabaseFactory().init()
+            embeddedServer(Netty, port = 8006) {
+                routing {
+                    get("/users") {
+                        call.respond("getUsers()")
+                    }
+                    post("/park") {
+                        val carNumber: String = Respond.parkingRequest(call.receiveText())
+                        println(carNumber)
+                        if (Validator.validateCarNumber(carNumber)) {
+                            // TODO: registerParking()
+                            call.response.status(HttpStatusCode.OK)
+                            call.respondText(Respond.parkingTimeRespond(DateTime.getCurrentDateAndTime()))
+                        } else {
+                            call.respond(HttpStatusCode(400, Respond.ICN))
+                        }
+
+
+                    }
+                    post("/unpark") {
+                        val carNumber: String = Respond.parkingRequest(call.receiveText())
+                        println(carNumber)
+                        if (Validator.validateCarNumber(carNumber)) {
+                            // TODO: checkIfCarIsNotParked
+                            // TODO: calculateSummary
+                            val dummySummary = 1000
+                            call.respondText(Respond.summaryRespond(dummySummary))
+                        } else {
+                            call.respond(HttpStatusCode(400, Respond.ICN))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/kotlin/database/DatabaseFactory.kt
+++ src/main/kotlin/database/DatabaseFactory.kt
@@ -1,12 +1,27 @@
 package database
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.jetbrains.exposed.sql.Database
 
-object DatabaseFactory {
+class DatabaseFactory {
 
     fun init() {
         val url = "jdbc:postgresql://localhost:5432/parking_service"
         val driver = "org.postgresql.Driver"
         Database.connect(url, driver, "evgenysobko", "12345")
+    }
+
+    private fun createConfig(): HikariDataSource {
+        HikariConfig().run {
+            driverClassName = "org.postgresql.Driver"
+            jdbcUrl = "jdbc:postgresql://localhost:5432/parking_service"
+            username = "evgenysobko"
+            password = "12345"
+            isAutoCommit = false
+            transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+            validate()
+            return HikariDataSource(this)
+        }
     }
 }
