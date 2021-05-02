b67b5d3bde2d7298f8036960d94662b307fbfe1d
evgenysobko <jooohn127@gmail.com>
2021-05-02 23:39:27 +0300

    final commit

--- src/main/kotlin/utils/CertificateProvider.kt
+++ src/main/kotlin/utils/CertificateProvider.kt
@@ -1,7 +1,5 @@
 package utils
 
-import io.ktor.network.tls.certificates.*
-import io.ktor.network.tls.extensions.*
 import io.ktor.server.engine.*
 import java.io.File
 import java.security.KeyStore
@@ -9,28 +7,13 @@ import java.security.KeyStore
 object CertificateProvider {
 
     fun ApplicationEngineEnvironmentBuilder.installCertificates() {
-        val keystore = buildKeyStore {
-            certificate(ALIAS) {
-                hash = HashAlgorithm.SHA256
-                sign = SignatureAlgorithm.ECDSA
-                keySizeInBits = 256
-                password = PASS
-            }
-        }
+        val keystore = KeyStore.getInstance(File(KEYSTORE), PASS.toCharArray())
         sslConnector(keystore, ALIAS, { PASS.toCharArray() }, { PASS.toCharArray() }) {
             port = 8007
-            keyStorePath = keystore.asFile.absoluteFile
         }
     }
 
-    private val KeyStore.asFile: File
-        get() {
-            val keyStoreFile = File(KEYSTORE)
-            this.saveToFile(keyStoreFile, PASS)
-            return keyStoreFile
-        }
-
     private const val ALIAS = "myalias"
     private const val PASS = "mypass"
-    private const val KEYSTORE = "/etc/letsencrypt/live/bulochka.duckdns.org/keystore.jks"
+    private const val KEYSTORE = "/Users/evgenysobko/Desktop/keystore.jks"
 }
